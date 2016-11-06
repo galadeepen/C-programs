edfe7ca35886a60a9ce28fbfc24ef6b88bb07ff4
deepen <galadeepen@utexas.edu>
2016-11-06 08:47:53 -0600

    Using member function in the simple program

--- prog2.c
+++ prog2.c
@@ -4,8 +4,16 @@
 int main()
 {
     Sales_item item1,item2;
-    std::cout<<"Enter 2 sales item with same isbn"<<std::endl;
+    std::cout<<"Enter 2 sales item whose sum you want"<<std::endl;
     std::cin>>item1>>item2;
-    std::cout<<"the sum is "<<item1+item2<<std::endl;
-    return 0;
+    if (item1.same_isbn(item2))
+    {
+        std::cout<<"the sum is "<<item1+item2<<std::endl;
+        return 0;
+    }
+    else
+    {
+        std::cerr<<"Please enter items with same isbn "<<std::endl;
+        return -1;
+    }
 }
